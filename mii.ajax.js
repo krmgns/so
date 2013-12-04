4e2acbccb1499fd9c0b38cb02b316a3855c99455
Kerem Güneş <qeremy@gmail.com>
2013-12-04 19:01:17 +0200

    Removed "Content-Length" cos chrome not-save error.

--- mii.ajax.js
+++ mii.ajax.js
@@ -170,7 +170,6 @@ Ajax.prototype = {
         // Set request header for POST etc.
         if (options.method != "GET" && options.data && options.data.length) {
             this._xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            this._xhr.setRequestHeader("Content-Length", options.data.length);
         }
 
         // Set request headers if exist
