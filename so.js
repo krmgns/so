df232761aac2796b2ecafd1c3aea9c6157d08a5e
Kerem Güneş <k-gun@mail.com>
2017-02-01 16:46:52 +0300

    Move $.browser bottom, update $.mix().

--- so.js
+++ so.js
@@ -82,78 +82,10 @@ function freeze(object, deep) {
     return Object.freeze(object);
 }
 
-var browser = (function() {
-    var foo, nav = window.navigator, re, name,
-        ua = nav.userAgent.toLowerCase(), uap = nav.platform.toLowerCase(),
-        browser = {}, src = /(chrome|safari|firefox|opera|msie|trident(?=\/))\/?\s*([\d.]+)/,
-        fns_os = ['isMac', 'isWindows', 'isLinux', 'isUnix'],
-        fns_ua = ['isChrome', 'isSafari', 'isFirefox', 'isOpera', 'isIE', 'isTrident'],
-        ret_isTouchDevice = nav.maxTouchPoints > 0 || 'ontouchstart' in window,
-        ret_isMobileDevice = /android|ip(hone|od|ad)|opera *mini|webos|blackberry|mobile|windows *phone/.test(ua);
-
-    browser.isTouchDevice = function() { return ret_isTouchDevice; };
-    browser.isMobileDevice = function() { return ret_isMobileDevice; };
-
-    function search(src, s) {
-        return src.indexOf(s) > -1;
-    }
-
-    // set 'is' functions for os
-    fns_os.forEach(function(fn) {
-        var osName = fn.slice(2).toLowerCase();
-        browser[fn] = (fn == 'isUnix')
-            ? function() { return search(ua, 'x11') && !search(ua, 'linux'); }
-            : function() { return search(ua, osName); };
-        // set os name testing
-        if (browser[fn]()) {
-            browser['osName'] = osName;
-        }
-    });
-
-    // set 'is' functions for browser
-    fns_ua.forEach(function(fn) {
-        browser[fn] = function() { return false; };
-    });
-
-    if (re = src.exec(ua)) {
-        if (re[1]) {
-            browser['name'] = (re[1] == 'msie') ? 'ie' : re[1];
-            // re-set 'is' function
-            browser['is'+ (browser['name'] == 'ie'
-                ? browser['name'].toUpperCase()
-                : browser['name'].toCapitalCase(false))] = function() { return true; };
-        }
-        if (re[2]) {
-            var versionArray = re[2].split('.').map(function(value, key) {
-                return value.toInt();
-            }), versionString;
-            versionString = versionArray.slice(0,2).join('.');
-            browser['version'] = versionString.toFloat();
-            browser['versionArray'] = versionArray;
-            browser['versionString'] = versionString;
-            browser['versionOrig'] = re[2];
-       }
-    }
-
-    if (browser['osName']) {
-        /* x86_64 x86-64 x64; amd64 amd64 wow64 x64_64 ia64 sparc64 ppc64 irix64
-            linux i386 linux i686 linux x86_64 win32 win64 macintel? */
-        foo = browser.isOpera() ? ua : uap;
-        if (/64/.test(foo)) {
-            browser['osBit'] = 64;
-        } else if (/32|86/.test(foo)) {
-            browser['osBit'] = 32;
-        }
-    }
-
-    return freeze(browser);
-})();
-
 var uuid = 0;
 
 /*** the so ***/
 var so = {
-    browser: browser,
     fun: function() {
         return function(){};
     },
@@ -235,15 +167,14 @@ var so = {
     },
     // notation: options = $.mix({}, defaultOptions, options);
     mix: function() {
-        var args = arguments, i = 1, target, source;
+        var args = arguments, i = 1, target = args[0], source;
         if (args.length < 2) {
-            throw ("so.mix(): Function accepts at least 2 arguments.");
+            throw ("$.mix() accepts at least 2 arguments!");
         }
 
-        target = args[0];
         while (source = args[i++]) {
             for (var key in source) {
-                source.hasOwnProperty(key) && (target[key] = source[key]);
+                if (source.hasOwnProperty(key)) target[key] = source[key];
             }
         }
 
@@ -253,8 +184,9 @@ var so = {
         var targetType = typeof target,
             sourceType = typeof source;
 
+        // self extend
         if (targetType == "object" && sourceType == "undefined") {
-            source = target, target = this; // self extend
+            source = target, target = this;
         } else if (targetType == "string") {
             target = !this[target] ? this[target] = {} : this[target];
         }
@@ -273,6 +205,73 @@ var so = {
         };
     }
 };
+
+so.browser = (function() {
+    var foo, nav = window.navigator, re, name,
+        ua = nav.userAgent.toLowerCase(), uap = nav.platform.toLowerCase(),
+        browser = {}, src = /(chrome|safari|firefox|opera|msie|trident(?=\/))\/?\s*([\d.]+)/,
+        fns_os = ['isMac', 'isWindows', 'isLinux', 'isUnix'],
+        fns_ua = ['isChrome', 'isSafari', 'isFirefox', 'isOpera', 'isIE', 'isTrident'],
+        ret_isTouchDevice = nav.maxTouchPoints > 0 || 'ontouchstart' in window,
+        ret_isMobileDevice = /android|ip(hone|od|ad)|opera *mini|webos|blackberry|mobile|windows *phone/.test(ua);
+
+    browser.isTouchDevice = function() { return ret_isTouchDevice; };
+    browser.isMobileDevice = function() { return ret_isMobileDevice; };
+
+    function search(src, s) {
+        return src.indexOf(s) > -1;
+    }
+
+    // set 'is' functions for os
+    fns_os.forEach(function(fn) {
+        var osName = fn.slice(2).toLowerCase();
+        browser[fn] = (fn == 'isUnix')
+            ? function() { return search(ua, 'x11') && !search(ua, 'linux'); }
+            : function() { return search(ua, osName); };
+        // set os name testing
+        if (browser[fn]()) {
+            browser['osName'] = osName;
+        }
+    });
+
+    // set 'is' functions for browser
+    fns_ua.forEach(function(fn) {
+        browser[fn] = function() { return false; };
+    });
+
+    if (re = src.exec(ua)) {
+        if (re[1]) {
+            browser['name'] = (re[1] == 'msie') ? 'ie' : re[1];
+            // re-set 'is' function
+            browser['is'+ (browser['name'] == 'ie'
+                ? browser['name'].toUpperCase()
+                : browser['name'].toCapitalCase(false))] = function() { return true; };
+        }
+        if (re[2]) {
+            var versionArray = re[2].split('.').map(function(value, key) {
+                return value.toInt();
+            }), versionString;
+            versionString = versionArray.slice(0,2).join('.');
+            browser['version'] = versionString.toFloat();
+            browser['versionArray'] = versionArray;
+            browser['versionString'] = versionString;
+            browser['versionOrig'] = re[2];
+       }
+    }
+
+    if (browser['osName']) {
+        /* x86_64 x86-64 x64; amd64 amd64 wow64 x64_64 ia64 sparc64 ppc64 irix64
+            linux i386 linux i686 linux x86_64 win32 win64 macintel? */
+        foo = browser.isOpera() ? ua : uap;
+        if (/64/.test(foo)) {
+            browser['osBit'] = 64;
+        } else if (/32|86/.test(foo)) {
+            browser['osBit'] = 32;
+        }
+    }
+
+    return freeze(browser);
+})();
 
 var callbacks = [];
 
