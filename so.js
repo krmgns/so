0d3ea1fb39596c5d38723bd64721cf3f310a75f4
Kerem Güneş <k-gun@mail.com>
2017-02-02 04:27:02 +0300

    Move $.browser.

--- so.js
+++ so.js
@@ -7,360 +7,274 @@ function log(s) { console.log.apply(console, arguments); }
 
 ;(function(window, undefined) {
 
-'use strict';
+    'use strict';
 
-// simple support check
-if (!''.trim) {
-    throw ('Archaic browser!');
-}
+    // simple support check
+    if (!''.trim) {
+        throw ('Archaic browser!');
+    }
 
-var so = {}, _uuid = 0, fn_toString = {}.toString;
+    var so = {ext: {}, array: {}, object: {}},
+        _uuid = 0, fn_toString = {}.toString;
 
-// base helpers
-function mix() {
-    var args = arguments, i = 1, target = args[0], source;
-    if (target) {
-        while (source = args[i++]) {
-            for (var key in source) {
-                if (source.hasOwnProperty(key)) {
-                    target[key] = source[key];
+    // base helpers
+    function mix() {
+        var args = arguments, i = 1, target = args[0], source;
+        if (target) {
+            while (source = args[i++]) {
+                for (var key in source) {
+                    if (source.hasOwnProperty(key)) {
+                        target[key] = source[key];
+                    }
                 }
             }
         }
+        return target;
     }
-    return target;
-}
 
-function extend(target, source) {
-    return mix(target, source);
-}
-
-function isNumeric(s) {
-    return !isNaN(parseFloat(s)) && isFinite(s);
-}
-
-// shorthand string helpers
-extend(String.prototype, {
-    toInt: function(base) {
-        return isNumeric(this) ? parseInt(this.replace(/^-?\.(.+)/, '0.\$1'), base || 10) : null;
-    },
-    toFloat: function() {
-        return isNumeric(this) ? parseFloat(this) : null;
-    },
-    isNumeric: function() {
-        return isNumeric(this);
-    },
-    format: function() {
-        var s = this, ms = s.match(/(%s)/g) || [], i = 0, m;
-        if (ms.length > arguments.length) {
-            throw ('No arguments enough!');
-        }
-        while (m = ms.shift()) {
-            s = s.replace(/(%s)/, arguments[i++]);
-        }
-        return s;
-    },
-    trimLeft: function(cs) {
-        var s = this, cs, re = cs;
-        if (cs.trim) { // charset given
-            cs = escapeRegExpInput(cs) || '\\s', re = new RegExp('^['+ cs +']+');
-        }
-        while (re.test(s)) {
-            s = s.replace(re, '');
-        }
-        return s;
-    },
-    trimRight: function(cs) {
-        var s = this, cs, re = cs;
-        if (cs.trim) { // charset given
-            cs = escapeRegExpInput(cs) || '\\s', re = new RegExp('['+ cs +']+$');
-        }
-        log(re, re.exec(s))
-        while (re.test(s)) {
-            s = s.replace(re, '');
-        }
-        return s;
-    },
-    trim: function(cs) {
-        return this.trimLeft(cs).trimRight(cs);
-    },
-    toCapitalCase: function(all) {
-        var s = this.toLowerCase(), i;
-        if (all !== false) {
-            for (i = 0, s = s.split(' '); i < s.length; i++) {
-                s[i] = s[i].toCapitalCase(false);
-            }
-            return s.join(' ');
-        }
-        return s.charAt(0).toUpperCase() + s.slice(1);
+    function extend(target, source) {
+        return mix(target, source);
     }
-});
 
-function forEach(input, fn, scope) {
-    var len = input && input.length, i;
-    if (len != null) { // array: value => i
-        for (i = 0; i < len; i++) {
-            if (false === fn.call(scope || input[i], input[i], i, input)) {
-                break;
+    function isNumeric(s) {
+        return !isNaN(parseFloat(s)) && isFinite(s);
+    }
+
+    // shorthand string helpers
+    extend(String.prototype, {
+        toInt: function(base) {
+            return isNumeric(this) ? parseInt(this.replace(/^-?\.(.+)/, '0.\$1'), base || 10) : null;
+        },
+        toFloat: function() {
+            return isNumeric(this) ? parseFloat(this) : null;
+        },
+        isNumeric: function() {
+            return isNumeric(this);
+        },
+        format: function() {
+            var s = this, ms = s.match(/(%s)/g) || [], i = 0, m;
+            if (ms.length > arguments.length) {
+                throw ('No arguments enough!');
             }
+            while (m = ms.shift()) {
+                s = s.replace(/(%s)/, arguments[i++]);
+            }
+            return s;
+        },
+        trimLeft: function(cs) {
+            var s = this, cs, re = cs;
+            if (cs && cs.trim || !re) {
+                cs = escapeRegExpInput(cs) || '\\s', re = new RegExp('^['+ cs +']+');
+            }
+            while (re.test(s)) {
+                s = s.replace(re, '');
+            }
+            return s;
+        },
+        trimRight: function(cs) {
+            var s = this, cs, re = cs;
+            if (cs && cs.trim || !re) {
+                cs = escapeRegExpInput(cs) || '\\s', re = new RegExp('['+ cs +']+$');
+            }
+            while (re.test(s)) {
+                s = s.replace(re, '');
+            }
+            return s;
+        },
+        trim: function(cs) {
+            return this.trimLeft(cs).trimRight(cs);
+        },
+        toCapitalCase: function(all) {
+            var s = this.toLowerCase(), i;
+            if (all !== false) {
+                for (i = 0, s = s.split(' '); i < s.length; i++) {
+                    s[i] = s[i].toCapitalCase(false);
+                }
+                return s.join(' ');
+            }
+            return s.charAt(0).toUpperCase() + s.slice(1);
         }
-    } else { // object: key => value
-        for (i in input) {
-            if (input.hasOwnProperty(i)) {
-                if (false === fn.call(scope || input[i], i, input[i], input)) {
+    });
+
+    function forEach(input, fn, opt_scope) {
+        var len = input && input.length, i;
+        if (len != null) { // array: value => i
+            for (i = 0; i < len; i++) {
+                if (false === fn.call(opt_scope || input[i], input[i], i, input)) {
                     break;
                 }
             }
-        }
-    }
-    return scope || input;
-}
-
-function freeze(object, deep) {
-    if (deep !== false) {
-        Object.getOwnPropertyNames(object).forEach(function(name) {
-            if (object[name] && typeof object[name] == 'object') {
-                freeze(object[name]);
-            }
-        });
-    }
-    return Object.freeze(object);
-}
-
-function escapeRegExpInput(input) {
-    return (''+ input).replace(/([\/\.\+\*\^\?\$\=\!\|\:\-\[\]\(\)\{\}\<\>\\])/g, '\\\$1');
-}
-
-function search(src, s) {
-    return src.indexOf(s) > -1;
-}
-
-/*** the so ***/
-extend(so, {
-    fun: function() {
-        return function(){};
-    },
-    now: function() {
-        return +(new Date);
-    },
-    uuid: function() {
-        return ++_uuid;
-    },
-    win: function(el) {
-        if (!el) return window;
-        if (el == el.window) return el;
-        var elType = el.nodeType;
-        if (elType == 1) {
-            el = el.ownerDocument; // find el document
-        }
-        return elType == 9 ? el.defaultView : null;
-    },
-    doc: function(el) {
-        return el ? el.ownerDocument : window.document;
-    },
-    trim: function(s, cs) {
-        return (s != null) ? s.trim(cs) : '';
-    },
-    trimLeft: function(s, cs) {
-        return (s != null) ? s.trimLeft(cs) : '';
-    },
-    trimRight: function(s, cs) {
-        return (s != null) ? s.trimRight(cs) : '';
-    },
-    dig: function(input, key) {
-        if (input && typeof input == 'object') {
-            var keys = (''+ key).split('.'), key = keys.shift();
-            if (!keys.length) {
-                return input[key];
-            }
-            return this.dig(input[key], keys.join('.'));
-        }
-    },
-    freeze: function(input, deep) {
-        return freeze(input, deep);
-    },
-    typeOf: function(x, real) {
-        if (x === null)              return 'null';
-        if (x === undefined)         return 'undefined';
-        if (real) {
-            if (isNumeric(x))        return 'numeric';
-            if (x.nodeType == 1)     return 'element';
-            if (x.nodeType == 9)     return 'document';
-        }
-        return fn_toString.call(x).slice(8, -1).toLowerCase();
-    },
-    isNone: function() {
-        var args = arguments, argsLen = args.length, i = 0;
-        if (argsLen) {
-            while (i < argsLen) {
-                if (args[i++] == null) {
-                    return true;
+        } else { // object: key => value
+            for (i in input) {
+                if (input.hasOwnProperty(i)) {
+                    if (false === fn.call(opt_scope || input[i], i, input[i], input)) {
+                        break;
+                    }
                 }
             }
-            return false;
         }
-    },
-    isSet: function(x, i) {
-        return (i == null) ? x != null : this.dig(x, i) != null;
-    },
-    isEmpty: function() {
-        var args = arguments, argsLen = args.length, i = 0, key, value;
-        if (argsLen) {
-            while (i < argsLen) {
-                value = args[i++];
-                if (!value) return true; // '', null, undefined, false, 0, NaN
-                if (typeof value.length == 'number') return !value.length;
-                if (typeof value == 'object') { for (key in value) { return false; } return true; }
+        return opt_scope || input;
+    }
+
+    function escapeRegExpInput(input) {
+        return (''+ input).replace(/([\/\.\+\*\^\?\$\=\!\|\:\-\[\]\(\)\{\}\<\>\\])/g, '\\\$1');
+    }
+
+    /*** the so ***/
+    extend(so, {
+        fun: function() {
+            return function(){};
+        },
+        now: function() {
+            return +(new Date);
+        },
+        uuid: function() {
+            return ++_uuid;
+        },
+        win: function(el) {
+            if (!el) return window;
+            if (el == el.window) return el;
+            var elType = el.nodeType;
+            if (elType == 1) {
+                el = el.ownerDocument; // find el document
             }
-            return false;
-        }
-    },
-    forEach: function(input, fn, scope) {
-        return forEach(input, fn, scope);
-    },
-    // notation: options = $.mix({}, defaultOptions, options);
-    mix: function() {
-        var args = arguments, i = 1, target = args[0], source;
-        if (args.length < 2) {
-            throw ("$.mix() accepts at least 2 arguments!");
-        }
+            return elType == 9 ? el.defaultView : null;
+        },
+        doc: function(el) {
+            return el ? el.ownerDocument : window.document;
+        },
+        trim: function(s, cs) {
+            return (s != null) ? s.trim(cs) : '';
+        },
+        trimLeft: function(s, cs) {
+            return (s != null) ? s.trimLeft(cs) : '';
+        },
+        trimRight: function(s, cs) {
+            return (s != null) ? s.trimRight(cs) : '';
+        },
+        dig: function(input, key) {
+            if (input && typeof input == 'object') {
+                var keys = (''+ key).split('.'), key = keys.shift();
+                if (!keys.length) {
+                    return input[key];
+                }
+                return this.dig(input[key], keys.join('.'));
+            }
+        },
+        freeze: function(object, opt_deep) {
+            if (opt_deep !== false) {
+                var _this = this;
+                Object.getOwnPropertyNames(object).forEach(function(name) {
+                    if (object[name] && typeof object[name] == 'object') {
+                        _this.freeze(object[name]);
+                    }
+                });
+            }
+            return Object.freeze(object);
+        },
+        typeOf: function(input, opt_real) {
+            if (input === null)              return 'null';
+            if (input === undefined)         return 'undefined';
+            if (opt_real) {
+                if (isNumeric(input))        return 'numeric';
+                if (input.nodeType == 1)     return 'element';
+                if (input.nodeType == 9)     return 'document';
+            }
+            return fn_toString.call(input).slice(8, -1).toLowerCase();
+        },
+        isNone: function() {
+            var args = arguments, argsLen = args.length, i = 0;
+            if (argsLen) {
+                while (i < argsLen) {
+                    if (args[i++] == null) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        },
+        isSet: function(input, opt_key) {
+            return (opt_key == null) ? input != null : this.dig(input, opt_key) != null;
+        },
+        isEmpty: function() {
+            var args = arguments, argsLen = args.length, i = 0, value;
+            if (argsLen) {
+                while (i < argsLen) {
+                    value = args[i++];
+                    if (!value) return true; // '', null, undefined, false, 0, NaN
+                    if (typeof value.length == 'number') return !value.length;
+                    if (typeof value == 'object') { !Object.keys(value).length; }
+                    // if (typeof value == 'object') { for (key in value) { return false; } return true; }
+                }
+                return false;
+            }
+        },
+        forEach: function() {
+            return forEach.apply(null, arguments);
+        },
+        // notation: options = $.mix({}, defaultOptions, options);
+        mix: function() {
+            if (arguments.length < 2) {
+                throw ('$.mix() accepts at least 2 arguments!');
+            }
 
-        return mix.apply(null, arguments);
-    },
-    extend: function(target, source) {
-        var targetType = typeof target,
-            sourceType = typeof source;
+            return mix.apply(null, arguments);
+        },
+        extend: function(target, source) {
+            var targetType = typeof target, sourceType = typeof source;
 
-        // self extend
-        if (targetType == "object" && sourceType == "undefined") {
-            source = target, target = this;
-        } else if (targetType == "string") {
-            target = !this[target] ? this[target] = {} : this[target];
-        }
+            // self extend
+            if (targetType == 'object' && sourceType == 'undefined') {
+                source = target, target = this;
+            } else if (targetType == 'string') {
+                target = !this[target] ? this[target] = {} : this[target];
+            }
 
-        return mix(target, source);
-    },
-    toString: function() {
-        var args = arguments;
-        if (!args.length) {
-            return "[object so]";
-        }
+            return mix(target, source);
+        },
+        toString: function(name, opt_object) {
+            if (!name) {
+                return '[object so]';
+            }
 
-        // define `toString` methods of modules
-        this[args[0]].toString = function() {
-            return "[object "+ args[1] +"]";
-        };
-    }
-});
+            var target = opt_object ? opt_object : this[name];
 
-// is* functions
-extend(so, {
-    isString: function(x) {},
-    // ...
-});
-
-// browser
-so.browser = (function() {
-    var foo, nav = window.navigator, re, name,
-        ua = nav.userAgent.toLowerCase(), uap = nav.platform.toLowerCase(),
-        browser = {}, re_src = /(chrome|safari|firefox|opera|msie|trident(?=\/))\/?\s*([\d.]+)/,
-        fns_os = ['isMac', 'isWindows', 'isLinux', 'isUnix'],
-        fns_ua = ['isChrome', 'isSafari', 'isFirefox', 'isOpera', 'isIE', 'isTrident'],
-        ret_isTouchDevice = nav.maxTouchPoints > 0 || 'ontouchstart' in window,
-        ret_isMobileDevice = /android|ip(hone|od|ad)|opera *mini|webos|blackberry|mobile|windows *phone/.test(ua);
-
-    browser.isTouchDevice = function() { return ret_isTouchDevice; };
-    browser.isMobileDevice = function() { return ret_isMobileDevice; };
-
-    // set 'is' functions for os
-    fns_os.forEach(function(fn) {
-        var osName = fn.slice(2).toLowerCase();
-        browser[fn] = (fn == 'isUnix')
-            ? function() { return search(ua, 'x11') && !search(ua, 'linux'); }
-            : function() { return search(ua, osName); };
-        // set os name testing
-        if (browser[fn]()) {
-            browser['osName'] = osName;
+            // define `toString` methods of modules
+            target.toString = function() {
+                return '[object so.'+ name +']';
+            };
         }
     });
 
-    // set 'is' functions for browser
-    fns_ua.forEach(function(fn) {
-        browser[fn] = function() { return false; };
+    // is* functions
+    extend(so, {
+        isString: function(x) {},
     });
 
-    if (re = re_src.exec(ua)) {
-        if (re[1]) {
-            browser['name'] = (re[1] == 'msie') ? 'ie' : re[1];
-            // re-set 'is' function
-            browser['is'+ (browser['name'] == 'ie'
-                ? browser['name'].toUpperCase()
-                : browser['name'].toCapitalCase(false))] = function() { return true; };
-        }
-        if (re[2]) {
-            var versionArray = re[2].split('.').map(function(value, key) {
-                return value.toInt();
-            }), versionString;
-            versionString = versionArray.slice(0,2).join('.');
-            browser['version'] = versionString.toFloat();
-            browser['versionArray'] = versionArray;
-            browser['versionString'] = versionString;
-            browser['versionOrig'] = re[2];
-       }
-    }
+    var callbacks = [];
 
-    if (browser['osName']) {
-        /* x86_64 x86-64 x64; amd64 amd64 wow64 x64_64 ia64 sparc64 ppc64 irix64
-            linux i386 linux i686 linux x86_64 win32 win64 macintel? */
-        foo = browser.isOpera() ? ua : uap;
-        if (/64/.test(foo)) {
-            browser['osBit'] = 64;
-        } else if (/32|86/.test(foo)) {
-            browser['osBit'] = 32;
+    function fireCallbacks() {
+        while (callbacks.length) {
+            callbacks.shift()(so);
         }
     }
 
-    return freeze(browser);
-})();
+    // on ready
+    so.onReady = function(callback, document) {
+        if (typeof callback == 'function') {
+            callbacks.push(callback);
+        }
 
-var callbacks = [];
+        // iframe support
+        document = document || window.document;
 
-function fireCallbacks() {
-    while (callbacks.length) {
-        callbacks.shift()(so);
-    }
-}
-
-// on ready
-so.onReady = function(callback, document) {
-    if (typeof callback == "function") {
-        callbacks.push(callback);
-    }
-
-    // iframe support
-    document = document || window.document;
-
-    // come on..
-    if (document.addEventListener) {
-        return document.addEventListener("DOMContentLoaded", function DOMContentLoaded(){
-            document.removeEventListener("DOMContentLoaded", DOMContentLoaded, false);
+        document.addEventListener('DOMContentLoaded', function DOMContentLoaded(){
+            document.removeEventListener('DOMContentLoaded', DOMContentLoaded, false);
             fireCallbacks();
         }, false);
-    }
-
-    // credits https://developer.mozilla.org/DOM/document.readyState
-    document.onreadystatechange = function() {
-        if (this.readyState == "complete") {
-            document.onreadystatechange = null;
-            fireCallbacks();
-        }
     };
-};
 
-// some more tools..
-so.ext = {}, so.array = {}, so.object = {};
-
-// `so` to window
-window.so = so;
+    // add global
+    window.so = so;
 
 })(window);
