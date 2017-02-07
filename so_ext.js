bf8d979afc319d70a15c42f4dcac316f66fced5a
Kerem Güneş <k-gun@mail.com>
2017-02-07 06:16:23 +0300

    Add docs.

--- so_ext.js
+++ so_ext.js
@@ -1,16 +1,28 @@
-/**
- * @name: so.ext
- * @deps: so
- */
-
 ;(function($) { 'use strict';
 
+    /**
+     * @package so
+     * @object  so.ext
+     * @depends so
+     * @author  Kerem Güneş <k-gun@mail.com>
+     */
     $.extend('@ext', {
+        /**
+         * toCamelCaseFromDashCase.
+         * @param  {String} input
+         * @return {String}
+         */
         toCamelCaseFromDashCase: function(input) {
             return (''+ input).replace(/-([a-z])/gi, function($0, $1) {
                 return $1.toUpperCase();
             });
         },
+
+        /**
+         * toDashCaseFromUpperCase.
+         * @param  {String} input
+         * @return {String}
+         */
         toDashCaseFromUpperCase: function(input) {
             return (''+ input).replace(/([A-Z])/g, function($0, $1) {
                 return '-'+ $1.toLowerCase();
