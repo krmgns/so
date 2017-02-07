787ae5a4eeb582eeefd07f4e4da70466c17a6e14
Kerem Güneş <k-gun@mail.com>
2017-02-07 07:28:26 +0300

    Change arg name.

--- so_array.js
+++ so_array.js
@@ -26,16 +26,16 @@
 
         /**
          * Find.
-         * @param  {Array} input
-         * @param  {Any}   search
-         * @param  {Any}   opt_retDefault
+         * @param  {Array}    input
+         * @param  {Function} fn
+         * @param  {Any}      opt_retDefault
          * @return {Any|undefined}
          */
-        find: function(input, search, opt_retDefault) {
+        find: function(input, fn, opt_retDefault) {
             var ret = opt_retDefault;
 
             $.forEach(input, function(value) {
-                if (search(value)) {
+                if (fn(value)) {
                     ret = value;
                     return false; // break
                 }
@@ -46,16 +46,16 @@
 
         /**
          * Find.
-         * @param  {Array} input
-         * @param  {Any}   search
-         * @param  {Any}   opt_retDefault
+         * @param  {Array}    input
+         * @param  {Function} fn
+         * @param  {Any}      opt_retDefault
          * @return {Int|undefined}
          */
-        findIndex: function(input, search, opt_retDefault) {
+        findIndex: function(input, fn, opt_retDefault) {
             var ret = opt_retDefault;
 
             $.forEach(input, function(value, key) {
-                if (search(value)) {
+                if (fn(value)) {
                     ret = key;
                     return false; // break
                 }
