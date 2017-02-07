09b5f396af77b7f8dcf0e4d8a49e4a8c2ebd4a5e
Kerem Güneş <k-gun@mail.com>
2017-02-07 05:14:36 +0300

    Add $.browser.getGeoposition().

--- so_browser.js
+++ so_browser.js
@@ -3,18 +3,18 @@
  * @deps: so
  */
 
-;(function($) {
-
-    'use strict';
+;(function(window, $) { 'use strict';
 
     $.browser = (function() {
-        var foo, nav = window.navigator, re, name,
+        var nav = window.navigator, re, name, test,
             ua = nav.userAgent.toLowerCase(), uap = nav.platform.toLowerCase(),
-            browser = {}, re_src = /(chrome|safari|firefox|opera|msie|trident(?=\/))\/?\s*([\d.]+)/,
+            re_src = /(chrome|safari|firefox|opera|msie|trident(?=\/))\/?\s*([\d.]+)/,
             fns_os = ['isMac', 'isWindows', 'isLinux', 'isUnix'],
             fns_ua = ['isChrome', 'isSafari', 'isFirefox', 'isOpera', 'isIE', 'isTrident'],
             ret_isTouchDevice = nav.maxTouchPoints > 0 || 'ontouchstart' in window,
-            ret_isMobileDevice = /android|ip(hone|od|ad)|opera *mini|webos|blackberry|mobile|windows *phone/.test(ua);
+            ret_isMobileDevice = /android|ip(hone|od|ad)|opera *mini|webos|blackberry|mobile|windows *phone/.test(ua),
+            browser = {}
+        ;
 
         browser.isTouchDevice = function() { return ret_isTouchDevice; };
         browser.isMobileDevice = function() { return ret_isMobileDevice; };
@@ -51,7 +51,7 @@
                     : browser['name'].toCapitalCase(false))] = function() { return true; };
             }
             if (re[2]) {
-                var versionArray = re[2].split('.').map(function(value, key) {
+                var versionArray = re[2].split('.').map(function(value) {
                     return value.toInt();
                 }), versionString;
                 versionString = versionArray.slice(0,2).join('.');
@@ -65,15 +65,28 @@
         if (browser['osName']) {
             /* x86_64 x86-64 x64; amd64 amd64 wow64 x64_64 ia64 sparc64 ppc64 irix64
                 linux i386 linux i686 linux x86_64 win32 win64 macintel? */
-            foo = browser.isOpera() ? ua : uap;
-            if (/64/.test(foo)) {
+            test = browser.isOpera() ? ua : uap;
+            if (/64/.test(test)) {
                 browser['osBit'] = 64;
-            } else if (/32|86/.test(foo)) {
+            } else if (/32|86/.test(test)) {
                 browser['osBit'] = 32;
             }
         }
 
-        return $.freeze(browser);
+        // geoposition
+        browser.getGeoposition = function(onDone, onFail, options) {
+            options = $.extend({}, {
+                timeout: 5000,
+                maximumAge: 0,
+                enableHighAccuracy: true
+            }, options);
+
+            nav.geolocation.getCurrentPosition(function(position, onFail, options){
+                onDone(position, position.coords.latitude, position.coords.longitude);
+            });
+        };
+
+        return browser;
     })();
 
-})(so);
+})(window, so);
