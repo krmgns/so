a3d2c0571cd291b63b5384a940d400a01f32cd37
Kerem Güneş <k-gun@mail.com>
2017-02-07 07:21:28 +0300

    Add docs.

--- so_array.js
+++ so_array.js
@@ -1,11 +1,20 @@
-/**
- * @name: so.array
- * @deps: so
- */
-
 ;(function($) { 'use strict';
 
+    /**
+     * @package so
+     * @object  so.browser
+     * @depends so
+     * @author  Kerem Güneş <k-gun@mail.com>
+     * @license The MIT License <https://opensource.org/licenses/MIT>
+     */
     $.extend('@array', {
+        /**
+         * Has.
+         * @param  {Array}   input
+         * @param  {Any}     search
+         * @param  {Boolean} strict
+         * @return {Boolean}
+         */
         has: function(input, search, strict) {
             for (var i = 0, len = input.length; i < len; i++) {
                 if (!strict ? search == input[i] : search === input[i]) {
@@ -14,6 +23,14 @@
             }
             return false;
         },
+
+        /**
+         * Find.
+         * @param  {Array} input
+         * @param  {Any}   search
+         * @param  {Any}   opt_retDefault
+         * @return {Any|undefined}
+         */
         find: function(input, search, opt_retDefault) {
             var ret = opt_retDefault;
 
@@ -26,6 +43,14 @@
 
             return ret;
         },
+
+        /**
+         * Find.
+         * @param  {Array} input
+         * @param  {Any}   search
+         * @param  {Any}   opt_retDefault
+         * @return {Int|undefined}
+         */
         findIndex: function(input, search, opt_retDefault) {
             var ret = opt_retDefault;
 
