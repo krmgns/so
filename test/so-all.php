dbd16b4fd780adbafc2ffa336e93e7142862278e
Kerem Güneş <k-gun@mail.com>
2017-03-08 03:15:40 +0300

    Update file names.

--- test/so-all.php
+++ test/so-all.php
@@ -1,20 +1,19 @@
 <?php
 $files = array(
-    'qwery.min',
-    'so',
-    'so.ext',
-    'so.array',
-    'so.object',
-    'so.browser',
-    'so.animate',
-    'so.event',
-    'so.dom',
-    'so.ajax',
+    'so.js',
+    'so_util.js',
+    'so_class.js',
+    'so_list.js',
+    'so_browser.js',
+    'so_http.js',
+    'so_event.js',
+    'so_animation.js',
+    'so_dom.js',
 );
 
 header('Content-Type: text/javascript');
 foreach ($files as $file) {
-    print "// {$file}.js ******************\n";
-    print file_get_contents("../{$file}.js");
+    print "// {$file} ******************\n";
+    print file_get_contents("../{$file}");
     print "\n\n";
 }
