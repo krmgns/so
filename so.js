21861a3b21a6f4c33c6bb8e06760219669c4d695
Kerem Güneş <k-gun@mail.com>
2017-09-09 21:12:16 +0300

    Fix $.dom select() [x-child issue].

--- so.js
+++ so.js
@@ -20,7 +20,7 @@
 
     // globals
     window.so = $;
-    window.so.VERSION = '5.17.4';
+    window.so.VERSION = '5.17.5';
     window.so[NAME_WINDOW] = window;
     window.so[NAME_DOCUMENT] = window[NAME_DOCUMENT];
 
