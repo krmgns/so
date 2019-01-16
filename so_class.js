a3ee799c4254a1dd6e04b4ca935e068cc0b65edd
Kerem Güneş <k-gun@mail.com>
2019-01-16 16:59:20 +0300

    Drop class.create(), fix/optimize class.extends().

--- so_class.js
+++ so_class.js
@@ -7,88 +7,35 @@
  */
 ;(function($) { 'use strict';
 
-    // minify candy
-    var NAME_PROTOTYPE = 'prototype';
-
-    // create constructor with original name (minify tools change the original name)
-    function createConstructor(name, contents) {
-        return new Function('return function '+ name +'(){'+ contents +'}')();
-    }
-
-    /**
-     * So class.
-     * @param  {Function} subClass For $.class.extends()
-     * @return {Object}
-     */
-    $.class = function(subClass) { return {
-        /**
-         * Create.
-         * @param  {String} name
-         * @param  {Object} prototype
-         * @usage  $.class.create('Foo', {...})
-         * @usage  $.class.create('Foo', {init: function() {...}, ...})
-         * @return {Function}
-         */
-        create: function(name, prototype) {
-            if ($.isObject(name)) {
-                prototype = name;
-                name = $.rid('__so_class_');
-            }
-
-            var Class, Constructor;
-
-            Class = createConstructor(name, 'if(this.init)this.init.apply(this,arguments)');
-            if (prototype) {
-                Class[NAME_PROTOTYPE] = Object.create(prototype, {
-                    constructor: {value: (function() {
-                        Constructor = createConstructor(name);
-                        Constructor[NAME_PROTOTYPE] = prototype;
-                        Constructor[NAME_PROTOTYPE].constructor = Constructor;
-                        return Constructor;
-                    })()}
-                });
-            }
-
-            return Class;
-        },
-
-        /**
-         * Extend.
-         * @param  {Function} target
-         * @param  {Object}   prototype
-         * @return {Function}
-         */
-        extend: function(target, prototype) {
-            return $.extend(target[NAME_PROTOTYPE], prototype);
-        },
-
+    var $class = function(subClass) { return {
         /**
          * Extends.
          * @param  {Function} supClass
-         * @param  {Function} subClass  From $.class(subClass)
-         * @usage  $.class(Foo).extends(FooBase)
          * @return {Function}
          */
         extends: function(supClass) {
-            // subClass.prototype = Object.create(supClass.prototype, {
-            //     constructor: {value: subClass},
-            //           super: {value: supClass}
-            // });
 
-            var prototype = $.extend({
-                constructor: subClass,
-                      super: supClass
-            }, supClass[NAME_PROTOTYPE], subClass[NAME_PROTOTYPE]);
+            // @note super() always must be called in subClass constructor
+            var prototype = $.extend({}, supClass.prototype, subClass.prototype,
+                {constructor: subClass, super: supClass});
 
-            $.forEach(prototype, function(name, value) {
-                subClass[NAME_PROTOTYPE][name] = value;
-            });
+            subClass.prototype = Object.create(prototype);
 
             return subClass;
         }
     }};
 
-    // add shortcuts for without ()'s.
-    $.class.create = $.class().create;
-    $.class.extend = $.class().extend;
+    /**
+     * Extend.
+     * @param  {Function} target
+     * @param  {Object}   prototype
+     * @return {Function}
+     */
+    $class.extend = function(target, prototype) {
+        return $.extend(target.prototype, prototype);
+    },
+
+    // export
+    $.class = $class;
+
 })(window.so);
